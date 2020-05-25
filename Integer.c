28831ea47e70c82a9062261c686813f295a6f83b
Liam <liam@jonathanmcguinness.com>
2020-05-25 21:57:43 +0800

    code on integers

---
+++ Integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myFirstInteger;
+    
+    myFirstInteger = 6;
+
+    printf("myFirstInteger = %d\n", myFirstInteger);
+
+    printf("noVariable = %d\n", 5);
+    
+    return 0;
+}
