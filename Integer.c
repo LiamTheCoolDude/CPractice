c575b28c372e8ab983add26dd9dd7893c4dd1a0d
Liam <liam@jonathanmcguinness.com>
2020-05-25 22:16:09 +0800

    my first C homework

--- Integer.c
+++ Integer.c
@@ -2,12 +2,12 @@
 
 int main() {
     int myFirstInteger;
-    
+    int mySecondInteger;
+
     myFirstInteger = 6;
-
-    printf("myFirstInteger = %d\n", myFirstInteger);
-
-    printf("noVariable = %d\n", 5);
+    mySecondInteger = 7;
+    
+    printf("noVariable = %d\nmyFirstInteger = %d\nmySecondInteger = %d\n", 5, myFirstInteger, mySecondInteger);
     
     return 0;
 }
