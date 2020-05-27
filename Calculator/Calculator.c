c82c14969b26b5e87f2e37787420edeaae3534d5
Liam <liam@jonathanmcguinness.com>
2020-05-27 14:06:09 +0800

    Mini Calculator

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -5,18 +5,17 @@ int main() {
     int myFirstInteger;
     int mySecondInteger;
     int result;
-    
-    //initialisations
-    myFirstInteger = 24;
-    mySecondInteger = 79;
-    result = 0;
 
+    //initialisations
+    myFirstInteger = 4;
+    mySecondInteger = 9;
+    
     //functions
     result = myFirstInteger * mySecondInteger;
     printf("The question is:\n");
-    printf(" What is %d * %d?\n", myFirstInteger ,mySecondInteger);
-    printf("The answer is:\n");
-    printf(" %d\n", result);
-    
+    printf("What is %d * %d\n", myFirstInteger ,mySecondInteger);
+    scanf("%d", &result);
+    printf("%d\n", myFirstInteger * mySecondInteger);
+
     return 0;
 }
