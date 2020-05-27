79ae1466fa03817e3ca4e605cf51af638833251a
ZenterFlow <ZenterFlow@users.noreply.github.com>
2020-05-28 00:31:48 +0800

    learn to basic bool

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -1,21 +1,33 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main() {
     //declarations
     int myFirstInteger;
     int mySecondInteger;
-    int result;
-
+    int input;
+    int answer;
+    bool correct = false;
+    
     //initialisations
     myFirstInteger = (myFirstInteger % 10) + 1;
     mySecondInteger = (mySecondInteger % 10) + 1;
     
     //functions
-    result = myFirstInteger * mySecondInteger;
+    answer = myFirstInteger * mySecondInteger;
+    
     printf("The question is:\n");
     printf("What is %d * %d\n", myFirstInteger ,mySecondInteger);
-    scanf("%d", &result);
-    printf("The correct answer is : %d\n", myFirstInteger * mySecondInteger);
+    scanf("%d", &input);
+
+    correct = (input == answer);
+
+    if (correct)
+        printf("Your answer was %s\n", "RIGHT");
+    else 
+        printf("Your answer was %s\n", "WRONG");
+
+    printf("The correct answer is %d\n", answer);
 
     return 0;
 }
