b57e456ed1b385db9bc058791046e22db48922ba
Liam <liam@jonathanmcguinness.com>
2020-05-28 09:34:55 +0800

    trying force

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -4,17 +4,16 @@ int main() {
     //declarations
     int myFirstInteger;
     int mySecondInteger;
-    int result;
+    float result;
 
     //initialisations
     myFirstInteger = (myFirstInteger % 10) + 1;
     mySecondInteger = (mySecondInteger % 10) + 1;
     
     //functions
-    result = myFirstInteger * mySecondInteger;
     printf("The question is:\n");
     printf("What is %d * %d\n", myFirstInteger ,mySecondInteger);
-    scanf("%d", &result);
+    scanf("%f", &result);
     printf("The correct answer is : %d\n", myFirstInteger * mySecondInteger);
 
     return 0;
