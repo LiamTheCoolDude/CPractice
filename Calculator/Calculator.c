247be8b30d3a801477f12e8ec7edc6ed156a11e3
Liam <liam@jonathanmcguinness.com>
2020-05-28 10:19:34 +0800

    Mini question maker

--- Calculator/Calculator.c
+++ Calculator/Calculator.c
@@ -5,7 +5,11 @@ int main() {
     //declarations
     int myFirstInteger;
     int mySecondInteger;
-    int answer;
+    int answerAdd;
+    int answerSubtract;
+    int answerMultiply;
+    int answerDivide;
+ 
     int input;
     bool correct = false;
     
@@ -14,20 +18,73 @@ int main() {
     mySecondInteger = (mySecondInteger % 10) + 1;
     
     //functions
-    answer = myFirstInteger * mySecondInteger;
+
+    //function add
+    answerAdd = myFirstInteger + mySecondInteger;
     
     printf("The question is:\n");
-    printf("What is %d * %d\n", myFirstInteger ,mySecondInteger);
+    printf("What is %d + %d\n", myFirstInteger ,mySecondInteger);
     scanf("%d", &input);
 
-    correct = (input == answer);
+    correct = (input == answerAdd);
 
     if (correct)
         printf("Your answer was %s\n", "RIGHT");
     else 
         printf("Your answer was %s\n", "WRONG");
 
-    printf("The correct answer is %d\n", answer);
+    printf("The correct answer is %d\n", answerAdd);
+    
+    //function Subtract
+    answerSubtract = myFirstInteger - mySecondInteger;   
+
+    printf("The subtraction question is:\n");
+    printf("What is %d - %d\n", myFirstInteger ,mySecondInteger);
+    scanf("%d", &input);
+
+    correct = (input == answerSubtract);
+
+    if (correct)
+        printf("Your answer was %s\n", "RIGHT");
+    else 
+        printf("Your answer was %s\n", "WRONG");
+
+    printf("The correct answer is %d\n", answerSubtract);
+
+    //function Multiply
+    answerMultiply = myFirstInteger * mySecondInteger;
+
+    printf("The multiplication question is:\n");
+    printf("What is %d * %d\n", myFirstInteger ,mySecondInteger);
+    scanf("%d", &input);
+
+    correct = (input == answerMultiply);
+
+    if (correct)
+        printf("Your answer was %s\n", "RIGHT");
+    else 
+        printf("Your answer was %s\n", "WRONG");
+
+    printf("The correct answer is %d\n", answerMultiply);
+
+    //function Divide
+    answerDivide = myFirstInteger / mySecondInteger;
+
+        printf("The question is:\n");
+    printf("What is %d / %d\n", myFirstInteger ,mySecondInteger);
+    scanf("%d", &input);
+
+    correct = (input == answerDivide);
+
+    if (correct)
+        printf("Your answer was %s\n", "RIGHT");
+    else 
+        printf("Your answer was %s\n", "WRONG");
+
+    printf("The correct answer is %d\n", answerDivide);
+    
+    
+
 
     return 0;
 }
